3b1b8b28ca249dd0f0e2c6fa35a156ae741ffe52
DMB-LEB <nhnative2020@gmail.com>
2023-09-25 14:11:36 -0400

    Added HOST and PORTto sequelize

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, process.env.HOST, process.env.PORT, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
